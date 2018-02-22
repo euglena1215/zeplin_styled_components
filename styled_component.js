6c3289c875d6730a1c70ac6bcc26d37d6a5e81e3
KentoMoriwaki <kento@wantedly.com>
2018-02-22 17:20:50 +0900

    Apply prettier

--- styled_component.js
+++ styled_component.js
@@ -9,7 +9,8 @@ function layer(context, layer) {
 */
 
 function layer(context, layer) {
-  const style =　layer.textStyles.length === 0 ? null : layer.textStyles[0].textStyle;
+  const style =
+    layer.textStyles.length === 0 ? null : layer.textStyles[0].textStyle;
   const code = `
 const component = styled.div\`
   ${width(layer)}
@@ -28,78 +29,101 @@ const component = styled.div\`
   ${opacity(layer)}
   ${backgroundColor(layer)}
 \`;
-`.split("\n").filter(line => !line.match(/^\s+$/)).join("\n");
-  
+`
+    .split("\n")
+    .filter(line => !line.match(/^\s+$/))
+    .join("\n");
+
   return {
     code: code,
     language: "javascript"
   };
-};
+}
 
 function width(layer) {
-  return `width: ${layer.rect.width}px;`
+  return `width: ${layer.rect.width}px;`;
 }
 
 function height(layer) {
-  return `height: ${layer.rect.height}px;`
+  return `height: ${layer.rect.height}px;`;
 }
 
 function fontFamily(style) {
-  if (style == null) return ""
-  return `font-family: ${style.fontFamily};`
+  if (style == null) return "";
+  return `font-family: ${style.fontFamily};`;
 }
 
 function fontSize(style) {
-  if (style == null) return ""
-  return `font-size: ${style.fontSize}px;`
+  if (style == null) return "";
+  return `font-size: ${style.fontSize}px;`;
 }
 
 function fontWeight(style) {
-  if (style == null) return ""
-  return `font-weight: ${style.fontWeight};`
+  if (style == null) return "";
+  return `font-weight: ${style.fontWeight};`;
 }
 
 function fontStyle(style) {
-  if (style == null) return ""
-  return style.fontStyle === "normal" ? "" : `font-style: ${style.fontStyle};`
+  if (style == null) return "";
+  return style.fontStyle === "normal" ? "" : `font-style: ${style.fontStyle};`;
 }
 
 function fontStretch(style) {
-  if (style == null) return ""
-  return style.fontStretch === "normal" ? "" : `font-stretch:    ${style.fontStretch};`
+  if (style == null) return "";
+  return style.fontStretch === "normal"
+    ? ""
+    : `font-stretch:    ${style.fontStretch};`;
 }
 
 function lineHeight(style) {
-  if (style == null) return ""
-  return `line-height: ${style.lineHeight / style.fontSize};`
+  if (style == null) return "";
+  return `line-height: ${style.lineHeight / style.fontSize};`;
 }
 
 function textAlign(style) {
-  if (style == null) return ""
-  return style.textAlign === "left" ? "" : `text-align: ${style.textAlign};`
+  if (style == null) return "";
+  return style.textAlign === "left" ? "" : `text-align: ${style.textAlign};`;
 }
 
 function color(style) {
-  if (style == null) return ""
-  return `color:  rgba(${style.color.r}, ${style.color.g}, ${style.color.b}, ${style.color.a});`
+  if (style == null) return "";
+  return `color:  rgba(${style.color.r}, ${style.color.g}, ${style.color.b}, ${
+    style.color.a
+  });`;
 }
 
 function opacity(layer) {
-  return layer.opacity === 1? "" : `opacity: ${layer.opacity};`
+  return layer.opacity === 1 ? "" : `opacity: ${layer.opacity};`;
 }
 
 function border(layer) {
-  return layer.borders.length === 0 ? "" : `border: solid ${JSON.stringify(layer.borders)};`
+  return layer.borders.length === 0
+    ? ""
+    : `border: solid ${JSON.stringify(layer.borders)};`;
 }
 
 function borderRadius(layer) {
-  return layer.borderRadius === 0 ? "" : `border-radius: ${layer.borderRadius};`
+  return layer.borderRadius === 0
+    ? ""
+    : `border-radius: ${layer.borderRadius};`;
 }
 
 function boxShadow(layer) {
-  return layer.shadows.length === 0 ? "" : `box-shadow: ${layer.shadows[0].offsetX}px ${layer.shadows[0].offsetY}px ${layer.shadows[0].blurRadius}px ${layer.shadows[0].spread}px rgba(${layer.shadows[0].color.r}, ${layer.shadows[0].color.g}, ${layer.shadows[0].color.b}, ${layer.shadows[0].color.a});`
+  return layer.shadows.length === 0
+    ? ""
+    : `box-shadow: ${layer.shadows[0].offsetX}px ${
+        layer.shadows[0].offsetY
+      }px ${layer.shadows[0].blurRadius}px ${layer.shadows[0].spread}px rgba(${
+        layer.shadows[0].color.r
+      }, ${layer.shadows[0].color.g}, ${layer.shadows[0].color.b}, ${
+        layer.shadows[0].color.a
+      });`;
 }
 
 function backgroundColor(layer) {
-  return layer.fills.length === 0 ? "" : `background-color: rgba(${layer.fills[0].color.r}, ${layer.fills[0].color.g}, ${layer.fills[0].color.b}, ${layer.fills[0].color.a})`
+  return layer.fills.length === 0
+    ? ""
+    : `background-color: rgba(${layer.fills[0].color.r}, ${
+        layer.fills[0].color.g
+      }, ${layer.fills[0].color.b}, ${layer.fills[0].color.a})`;
 }
